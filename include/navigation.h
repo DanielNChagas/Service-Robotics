db9bfc4e6547383b0e4d346d88105eaf5cf76207
Oskar <oskarw99@gmail.com>
2021-11-29 17:30:36 +0100

    New explore path

--- include/navigation.h
+++ include/navigation.h
@@ -2,7 +2,8 @@
 #define NAVIGATION_H
 
 #define NUM_NODES 17
-#define MAX_NUM_TURNS 20
+#define NUM_EXPLORE_PATH_TURNS 60
+#define MAX_NUM_TURNS NUM_EXPLORE_PATH_TURNS
 
 class Navigator
 {
@@ -23,9 +24,70 @@ private:
         int getNode(int t, int searchDirection);
 
     private:
-        static constexpr int numExplorePathTurns = 20;
-        static constexpr int explorePathTurns[20][2] = {
-            {3, 16}, {1, -1}, {-1, 14}, {0, 13}, {1, -1}, {1, -1}, {1, -1}, {-1, -1}, {1, 3}, {0, 4}, {-1, -1}, {0, 7}, {0, 8}, {1, -1}, {1, 12}, {0, 11}, {-1, 10}, {1, -1}, {-1, 14}, {-1, -1}};
+        static constexpr int numExplorePathTurns = NUM_EXPLORE_PATH_TURNS;
+        
+        static constexpr int explorePathTurns[NUM_EXPLORE_PATH_TURNS][2] = {
+            {3, 16},
+            {1, -1},
+            {-1, 14},
+            {-1, 13},
+            {1, -1},
+            {2, 15},
+            {-1, -1},
+            {-1, 13},
+            {1, -1},
+            {1, -1},
+            {1, -1},
+            {-1, -1},
+            {-1, 3},
+            {2, 1},
+            {0, 3},
+            {0, 4},
+            {-1, -1},
+            {-1, 7},
+            {-1, 5},
+            {1, -1},
+            {1, -1},
+            {2, 1},
+            {-1, -1},
+            {-1, -1},
+            {-1, 5},
+            {1, -1},
+            {-1, -1},
+            {-1, -1},
+            {2, 2},
+            {1, -1},
+            {1, -1},
+            {-1, -1},
+            {0, 5},
+            {-1, 7},
+            {-1, 8},
+            {1, -1},
+            {2, 6},
+            {-1, -1},
+            {-1, 8},
+            {1, -1},
+            {1, 12},
+            {-1, 11},
+            {1, -1},
+            {-1, -1},
+            {-1, -1},
+            {-1, -1},
+            {-1, 12},
+            {0, 11},
+            {0, 10},
+            {2, 9},
+            {1, 10},
+            {1, -1},
+            {0, 14},
+            {1, 13},
+            {1, -1},
+            {2, 4},
+            {-1, -1},
+            {-1, 13},
+            {1, 14},
+            {-1, -1},
+        };
 
         static constexpr int numHomePathTurns[NUM_NODES] = {7, 13, 14, 6, 5, 10, 10, 9, 8, 5, 4, 5, 6, 3, 2, 5};
         static constexpr int homePathTurns[NUM_NODES - 1][14][2] = {
