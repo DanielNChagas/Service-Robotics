b4a8187bda51ecbbf8f02de8ca240d939ba1fa99
Oskar <oskarw99@gmail.com>
2021-12-12 16:08:32 +0100

    Navigation change, pick up first person

--- oskar_test/navigation_test.cpp
+++ oskar_test/navigation_test.cpp
@@ -6,22 +6,29 @@ int main()
 {
 
     Navigator navigator;
+    //navigator.setPath(PATH_FIRST_PERSON);
+    cout<<"PATH_FIRST_PERSON\n";
 
-    int i = 0;
-
-    cout << "Exploring\n";
-    while (navigator.hasNextTurn())
+    for (int j = 0; j < 2; j++)
     {
-        if (i == 20) // Arbitrary, this is where we find the last person
-        {
-            cout << "Going home\n";
-            navigator.goHome();
-        }
+        int i = 0;
 
-        cout << navigator.nextTurn() << "\n";
-        i++;
+        cout << "Path #"<<j<<"\n";
+        while (navigator.hasNextTurn())
+        {
+            if (i == 15 && j == 1) // Arbitrary, this is where we find the last person
+            {
+                cout<<"PATH_HOME\n";
+                navigator.setPath(PATH_HOME);
+            }
+
+            cout << navigator.nextTurn() << "\n";
+            i++;
+        }
+        cout << "Path complete\n";
+        navigator.setPath(PATH_EXPLORE);
+        cout<<"PATH_EXPLORE\n";
     }
-    cout << "Home\n";
 
     cout << "\nNavigation test ended\n";
     return 0;
